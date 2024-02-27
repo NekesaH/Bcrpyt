35b51218b39e37778fa61f714b640832753f3f29
NekesaH <nyongesacharity13@gmail.com>
2024-02-27 10:47:35 +0300

    bcrypt

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, 13, (err, hash) => {
+    console.log(hash);
+    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res); //true
+    });
+  });
+  
 
 //END_ASYNC
 
